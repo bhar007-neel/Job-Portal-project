58bda16e32cf10a8f08757eb92be7eec9fd9c373
Neelmani Bhardwaj <neelmanibhardwaj003@gmail.com>
2025-07-09 15:34:08 -0400

    adding update jobs

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -1,12 +1,36 @@
 import jobsModel from "../models/jobsModel.js";
-
-export const createJobController = async(req,res,next)=>{
-   const { company, position } = req.body;
+//=============  CREATE JOBS ======================
+export const createJobController = async (req, res, next) => {
+  const { company, position } = req.body;
   if (!company || !position) {
     next("Please Provide All Fields");
   }
   req.body.createdBy = req.user.userId;
   const job = await jobsModel.create(req.body);
   res.status(201).json({ job });
-;}
-export const getAll
+};
+
+
+
+// =================== GET JOBS ========================================
+export const getAllJobsController = async (req, res, next) => {
+  const jobs = await jobsModel.find({ createdBy: req.user.userId });
+  res.status(200).json({
+    totalJobs: jobs.length,
+    jobs,
+  });
+};
+
+
+// ======================= UpDATE JOBS ===========================================
+
+export const updateJobController = async (req,res,next)=>{
+const {id} =req.params
+const {company,position}= req.body
+//validation
+if(!company || !position){
+    next('please provide all fields')
+}
+//find job
+const job = await jobsModel.findOne({_id:id})
+}
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,10 +1,13 @@
 import express from "express"
-import {createJobController} from '../controller/jobsController.js'
+import {createJobController,getAllJobsController} from '../controller/jobsController.js'
 import userAuth from "../middelwares/authmiddleware.js"
 const router = express.Router()
 // routes
 //create job|| Post
 router.post("/create-job",userAuth,createJobController);
 
-router.get('get-jobs',userAuth,)
+router.get('/get-jobs',userAuth,getAllJobsController)
 export default router
+
+//UPdate jobs || put
+router.patch("/update-job/:id",userAuth, updateJobController)
