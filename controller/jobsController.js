93e004e1f16601e0dda33b6908a9e79f33f985ad
Neelmani Bhardwaj <neelmanibhardwaj003@gmail.com>
2025-07-10 22:31:42 -0400

    adding delete and update jobs

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -10,8 +10,6 @@ export const createJobController = async (req, res, next) => {
   res.status(201).json({ job });
 };
 
-
-
 // =================== GET JOBS ========================================
 export const getAllJobsController = async (req, res, next) => {
   const jobs = await jobsModel.find({ createdBy: req.user.userId });
@@ -21,16 +19,48 @@ export const getAllJobsController = async (req, res, next) => {
   });
 };
 
-
 // ======================= UpDATE JOBS ===========================================
 
-export const updateJobController = async (req,res,next)=>{
-const {id} =req.params
-const {company,position}= req.body
-//validation
-if(!company || !position){
-    next('please provide all fields')
-}
-//find job
-const job = await jobsModel.findOne({_id:id})
-}
+export const updateJobController = async (req, res, next) => {
+  const { id } = req.params;
+  const { company, position } = req.body;
+  //validation
+  if (!company || !position) {
+    return next("please provide all fields");
+  }
+  //find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`no jobs found with this id ${id}`);
+  }
+  if (job.createdBy.toString() !== req.user.userId) {
+    return next("Not authorized");
+  }
+
+  const updateJob = await jobsModel.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  // response
+  res.status(200).json({ updateJob });
+};
+
+// =====================  DELETE job =========================================================================
+
+export const deleteJobController = async (req, res, next) => {
+  const { id } = req.params;
+  // find job
+  const job = await jobsModel.findOne({ _id: id });
+  //validation
+  if (!job) {
+    next(`no job found`);
+  }
+  if(!req.user.userId === job.createdBy.toString()){
+    next(`you are not authorzie to delete this job`)
+    return;
+  }
+
+  await jobsModel.findByIdAndDelete(id);
+    res.status(200).json({ message:"succesfully deleted"});
+};
--- routes/jobsRoute.js
+++ routes/jobsRoute.js
@@ -1,5 +1,6 @@
 import express from "express"
-import {createJobController,getAllJobsController} from '../controller/jobsController.js'
+import { createJobController, getAllJobsController, updateJobController,deleteJobController } from "../controller/jobsController.js";
+
 import userAuth from "../middelwares/authmiddleware.js"
 const router = express.Router()
 // routes
@@ -9,5 +10,9 @@ router.post("/create-job",userAuth,createJobController);
 router.get('/get-jobs',userAuth,getAllJobsController)
 export default router
 
-//UPdate jobs || put
+//UPdate jobs || put || patch
 router.patch("/update-job/:id",userAuth, updateJobController)
+
+//DELETE JOBS || DELETE
+
+router.delete("/delete-job/:id",userAuth,deleteJobController)
